45ad46ad668080d4f16d1ee2c66f2bd81b696240
Shoma Tadokoro <syou66ma@yahoo.co.jp>
2020-04-23 18:28:08 +0900

    setting rspec&factoryBot
    factoryBotをrspecで使用できるようにしました。

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :user do
-    
+    nickname              {"abe"}
+    birth_date            {"1992-10-1"}
+    email                 {"kkk@gmail.com"}
+    password              {"12345678"}
+    password_confirmation {"12345678"}
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  
+  it '有効なユーザー' do 
+    user = build(:user)
+    expect(user).to be_valid
+  end
+  
 end
