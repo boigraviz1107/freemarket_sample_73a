ad46f88fecf299a854568b119473615b421108b0
shun255454 <donp3461@gmail.com>
2020-04-25 18:37:08 +0900

    Account-specTest
    アカウントテスト一回目

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,8 +1,16 @@
 class Account < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, optional: true
   validates :post_num, presence: true, length: { is: 7 } 
   validates :prefecture, presence: true
   validates :city, presence: true
-  validates :city_num, presence: true
+  validates :city_num, presence: true 
+  VALID_PHONE_REGEX = /\A\d{10}$|^\d{11}\z/
+  validates :phone_num, presence: true, format: { with: VALID_PHONE_REGEX }
+  
+ 
+
 
 end
+
+
+
--- spec/factories/accounts.rb
+++ spec/factories/accounts.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :account do
-    
+    post_num              {"1234567"}
+    prefecture            {"兵庫県"}
+    city                  {"伊丹市"}
+    city_num              {"333番地"}
+    building              {""}
+    phone_num             {"0901234567"}
+    user_id               {"1"}
   end
 end
