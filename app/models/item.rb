174b005388ab08153312074d786aaafe6c191c3c
Shoma Tadokoro <syou66ma@yahoo.co.jp>
2020-04-28 00:01:27 +0900

    setting association
    アソシエーションを各モデルファイルに記載しました

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,10 @@
 class Item < ApplicationRecord
+  belongs_to :user
+  has_one :order
+  # has_many :comments,dependent: :destroy
+  has_many :images,dependent: :destroy
+  belongs_to :brand
+  belongs_to :category
+  validates :user_id, :category_id, :brand_id, :name, :explannation, :status, :shipper, :shipping_area, :lead_time, :price, :size, :shipping_method, presence: true
 end
+
