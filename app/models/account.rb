f8c829fa5137181c2d403f928a7059ff409430dd
taishu-mitsufuji <taishu.mitsufuji@gmail.com>
2020-04-27 12:43:26 +0900

    file_delete
    不要なファイルを削除。

--- app/models/account.rb
+++ app/models/account.rb
@@ -5,12 +5,6 @@ class Account < ApplicationRecord
   validates :city, presence: true
   validates :city_num, presence: true 
   VALID_PHONE_REGEX = /\A\d{10}$|^\d{11}\z/
+  # 上記は電話番号の10桁または11桁のみ登録できるようにする正規表現です
   validates :phone_num, presence: true, format: { with: VALID_PHONE_REGEX }
-  
- 
-
-
 end
-
-
-
