7a0a524049dd7f57817e6942877539b192bf42f6
taishu-mitsufuji <taishu.mitsufuji@gmail.com>
2020-04-28 15:56:07 +0900

    mypage_markup
    マイページ、ログアウトページ、カード登録ページのマークアップをした。

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'tops#index'
   resources :accounts, only: [:new, :create]
+  resources :users, only: [:index, :card, :logout]
+  get 'users/card', to: 'users#card'
+  get 'users/logout', to: 'users#logout'
 end
