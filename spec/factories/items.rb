df394b967174d26fde4ff803d6cb8a9d4ab4f4e6
Shoma Tadokoro <syou66ma@yahoo.co.jp>
2020-04-28 18:19:34 +0900

    rspec test Moel category&item&imagebrand

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,21 @@
 FactoryBot.define do
-  factory :item do
-    
+  factory :item, class: Item do
+    name { Faker::Name.name }
+    user { FactoryBot.create(:user, nickname:"nameTest", email:"abcde@gmail.com") }
+    category { FactoryBot.create :category }
+    brand { FactoryBot.create :brand }
+    explannation { Faker::Device.manufacturer }
+    status { Faker::Device.model_name  }
+    shipper { Faker::Boolean.boolean }
+    shipping_area { Prefecture.find(1).name }
+    lead_time { Faker::Number.between(from: 1, to: 3) }
+    price { Faker::Number.between(from: 300, to: 9999999) }
+    size { Faker::Military.army_rank }
+    shipping_method { Faker::University.name }
+    images {[
+      FactoryBot.build(:image, item: nil),
+      FactoryBot.build(:image, item: nil),
+      FactoryBot.build(:image, item: nil),
+    ]}
   end
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it '有効なcategory'do
+   expect(build(:category)).to be_valid
+  end
+  it 'nameが空だと保存できない'do
+   expect(build(:category,name:"")).not_to be_valid
+  end
 end
