07a165f6e3dfa31da92c8eaa654b818df3e84604
Shoma Tadokoro <syou66ma@yahoo.co.jp>
2020-05-01 14:09:06 +0900

    fix week1 task
    細かなバグを修正していきました。

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,21 +1,22 @@
 class CategoriesController < ApplicationController
-  
+
   def index
     @categories = Category.all.order("id ASC").limit(4)
   end
-  
+
   def show
     @category = Category.find(params[:id])
-    # @categories = Category.all.order("id ASC")
     @items = []
     if @category.is_root? && @category.is_childless? # 親も子もいない子
-      @items = @category.items
+      @items = @category.items.page(params[:page]).per(10)
     elsif @category.is_root? # 親の時
       indirects = @category.indirects
       indirects.each do |indirect|
         indirect.items.each do |item|
-          @items = @items.push(item)
+          # Item.where(category_id: indirect.id).order("created_at: DESC")
+          @items.push(item)
         end
+        @items = Kaminari.paginate_array(@items).page(params[:page]).per(10)
       end
     elsif @category.has_parent? && @category.has_children? # 子の時
       children = @category.children
@@ -24,12 +25,10 @@ class CategoriesController < ApplicationController
           @items = @items.push(item)
         end
       end
-    else # 孫
-      @items = @category.items
+      @items = Kaminari.paginate_array(@items).page(params[:page]).per(10)
+    else
+      @items = @category.items.page(params[:page]).per(10)
     end
-    @items = @items.page(params[:page]).per(6)
-    # 新しい順にしたい
-    # @items = @items.order(created_at: )
   end
 
 
