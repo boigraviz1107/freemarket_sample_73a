206a0b1035195c378295028e8c0724ace1f8b59c
taishu-mitsufuji <taishu.mitsufuji@gmail.com>
2020-05-04 23:03:01 +0900

    order_testCord_fixed
    orderモデルのテストコードの修正をしました。

--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -1,6 +1,9 @@
 FactoryBot.define do
   factory :order do
-    user_id { 1 }
-    item_id { 1 }
+    user_id { FactoryBot.create(:user) }
+    item_id { FactoryBot.create(:item) }
+    # user_id { 1 }
+    # item_id { 1 }
+    # association :item, :user
   end
 end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -3,26 +3,34 @@ require 'rails_helper'
 RSpec.describe Order, type: :model do
 
   it '有効なオーダー' do 
-    order = build(:order)
+    item = create(:item)
+    user = create(:user)
+    order = build(:order, user_id: user.id, item_id: item.id)
     expect(order).to be_valid
   end
 
   it "item_idがない場合は登録できないこと" do
-    order = build(:order, item_id: nil)
+    item = create(:item)
+    user = create(:user)
+    order = build(:order, user_id: user.id, item_id: nil)
     order.valid?
     expect(order.errors[:item_id]).to include("can't be blank")
   end
 
   it "user_idがない場合は登録できないこと" do
-    order = build(:order, user_id: nil)
+    item = create(:item)
+    user = create(:user)
+    order = build(:order, user_id: nil, item_id: item.id)
     order.valid?
     expect(order.errors[:user_id]).to include("can't be blank")
   end
 
   it '同じアイテムがすでにレコードにある場合に登録できない' do 
-    order = create(:order)
-    another_order = build(:order, user_id: "2")
-    another_user.valid?
+    item = create(:item)
+    user = create(:user)
+    order = create(:order, user_id: user.id, item_id: item.id)
+    another_order = build(:order, user_id: "3", item_id: item.id)
+    another_order.valid?
     expect(another_order.errors[:item_id]).to include("has already been taken")
     end
 
