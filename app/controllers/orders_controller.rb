200cab777ea891f2bb775f395e81088c51798a00
Shoma Tadokoro <syou66ma@yahoo.co.jp>
2020-05-04 19:11:00 +0900

    add validation
    パリデーションとアソシエーションを記載

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,20 +1,20 @@
 class OrdersController < ApplicationController
 
+  before_action :set_item
+  before_action :authenticate_user!
+  before_action :redirect_exhibitor
+
   def new
     @order = Order.new
-    @item = Item.find(params[:item_id])
   end
 
   def create
-    binding.pry
     @order = Order.new(params_order)
-      if @order.save
-        flash[:notice] = "購入が完了しました"
-        redirect_to root_path
-      else
-        session[:order] = @order
-        redirect_to new_item_order_path, flash: { errors: @order.errors.full_messages }
-      end
+    if @order.save
+      redirect_to root_path, flash: { notice: "購入が完了しました" }
+    else
+      redirect_to new_item_order_path, flash: { errors: @order.errors.full_messages }
+    end
   end
 
   private
@@ -23,4 +23,11 @@ class OrdersController < ApplicationController
     params.require(:order).permit(:item_id).merge(user_id: current_user.id)
   end
 
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def redirect_exhibitor
+    redirect_to users_path if @item.user == current_user
+  end
 end
