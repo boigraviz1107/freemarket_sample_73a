6f2939bab20f2da287ba3d7f544e9bc6c218d069
Shoma Tadokoro <syou66ma@yahoo.co.jp>
2020-05-04 11:09:47 +0900

    fix path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,7 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_in_path_for(resource)
-    users_path
+    new_account_path
   end
 
   def after_sign_out_path_for(resource)
