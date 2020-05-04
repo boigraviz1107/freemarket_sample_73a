34b2987de84845e0de75f388b901237840106e6e
boigraviz1107 <soccer.117@icloud.com>
2020-05-04 15:49:09 +0900

    pays_create_server
    カードの新規登録実装

--- app/controllers/pays_controller.rb
+++ app/controllers/pays_controller.rb
@@ -1,19 +1,42 @@
 class PaysController < ApplicationController
-
+require 'payjp'
+before_action :setcard
   def index
     # マイページからアクセス
   end
 
   def new
-    # カードを登録画面
   end
 
   def create
-    # カードを登録処理
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    if params['payjp-token'].blank?
+      render new
+    else
+      customer = Payjp::Customer.create(
+        email: current_user.email,
+        card: params['payjp-token'],
+      )
+      @card = Pay.create(
+        user_id: current_user.id, 
+        customer_id: customer.id, 
+        card_id: customer.cards.data[0].id
+      )
+      if @card.save
+        redirect_to  users_path
+      else
+        render :new
+      end
+    end
   end
 
   def destroy
     # indexから削除可能にする
   end
 
+  private
+  def setcard
+    @card = Pay.where(user_id:current_user.id)
+  end
+  
 end
