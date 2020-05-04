434dbdb77c5eac17e22556dbc06064d89202e1a3
taishu-mitsufuji <taishu.mitsufuji@gmail.com>
2020-05-04 18:18:04 +0900

    order_function
    orderモデルの機能を実装。

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,10 +1,26 @@
 class OrdersController < ApplicationController
 
   def new
+    @order = Order.new
+    @item = Item.find(params[:item_id])
   end
 
   def create
-    # 決済とオーダーレコードを追加
+    binding.pry
+    @order = Order.new(params_order)
+      if @order.save
+        flash[:notice] = "購入が完了しました"
+        redirect_to root_path
+      else
+        session[:order] = @order
+        redirect_to new_item_order_path, flash: { errors: @order.errors.full_messages }
+      end
+  end
+
+  private
+
+  def params_order
+    params.require(:order).permit(:item_id).merge(user_id: current_user.id)
   end
 
 end
