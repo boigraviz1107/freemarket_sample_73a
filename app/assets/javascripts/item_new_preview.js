b5fb24669a3a08a59a8a3f2903e8da36f4cdc91f
higeneko5324 <tetsu5324@icloud.com>
2020-05-07 14:08:59 +0900

    second-comit

--- app/assets/javascripts/item_new_preview.js
+++ app/assets/javascripts/item_new_preview.js
@@ -68,6 +68,60 @@
     //querySelectorでfile_fieldを取得
     var file_field = document.querySelector('input[type=file]')
     //fileが選択された時に発火するイベント
+    $('#item_images_attributes_1_image').change(function(){
+      //選択したfileのオブジェクトをpropで取得
+      var file = $('input[type="file"]').prop('files')[0];
+  
+      //FileReaderのreadAsDataURLで指定したFileオブジェクトを読み込む
+      var fileReader = new FileReader();
+  
+      
+      //読み込みが完了すると、srcにfileのURLを格納
+      fileReader.onloadend = function() {
+        var src = fileReader.result
+        var html= `<img src="${src}" width="100" height="95">`
+        //image_box__container要素の前にhtmlを差し込む
+        
+        $('#img_file').after(html);
+        var num = $('.img_file').length + 1 + i
+        if (num == 5){
+          $('#img_file').css('display', 'none')   
+        }
+      }
+      fileReader.readAsDataURL(file);
+    });
+ 
+    var dataBox = new DataTransfer();
+    //querySelectorでfile_fieldを取得
+    var file_field = document.querySelector('input[type=file]')
+    //fileが選択された時に発火するイベント
+    $('#item_images_attributes_2_image').change(function(){
+      //選択したfileのオブジェクトをpropで取得
+      var file = $('input[type="file"]').prop('files')[0];
+  
+      //FileReaderのreadAsDataURLで指定したFileオブジェクトを読み込む
+      var fileReader = new FileReader();
+  
+      
+      //読み込みが完了すると、srcにfileのURLを格納
+      fileReader.onloadend = function() {
+        var src = fileReader.result
+        var html= `<img src="${src}" width="100" height="95">`
+        //image_box__container要素の前にhtmlを差し込む
+        
+        $('#img_file').after(html);
+        var num = $('.img_file').length + 1 + i
+        if (num == 5){
+          $('#img_file').css('display', 'none')   
+        }
+      }
+      fileReader.readAsDataURL(file);
+    });
+  
+    var dataBox = new DataTransfer();
+    //querySelectorでfile_fieldを取得
+    var file_field = document.querySelector('input[type=file]')
+    //fileが選択された時に発火するイベント
     $('#item_images_attributes_0_image').change(function(){
       //選択したfileのオブジェクトをpropで取得
       var file = $('input[type="file"]').prop('files')[0];
