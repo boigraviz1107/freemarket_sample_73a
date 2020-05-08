345573074c515cf160e4c4a08d0b56caa0392e1a
Shoma Tadokoro <syou66ma@yahoo.co.jp>
2020-05-08 15:05:45 +0900

    image preview function

--- app/assets/javascripts/item_new_preview.js
+++ app/assets/javascripts/item_new_preview.js
@@ -1,25 +1,11 @@
 $(function () {
-  $('#item_images_attributes_0_image').on('change', function (e) {
+  function addImage(idName, en) {
     var reader = new FileReader();
-    reader.onload = function (e) {
-      $("#preview").attr('src', e.target.result);
-    }
-    reader.readAsDataURL(e.target.files[0]);
-  });
-
-  $('#item_images_attributes_1_image').on('change', function (e) {
-    var reader = new FileReader();
-    reader.onload = function (e) {
-      $("#preview1").attr('src', e.target.result);
-    }
-    reader.readAsDataURL(e.target.files[0]);
-  });
-
-  $('#item_images_attributes_2_image').on('change', function (e) {
-    var reader = new FileReader();
-    reader.onload = function (e) {
-      $("#preview2").attr('src', e.target.result);
-    }
-    reader.readAsDataURL(e.target.files[0]);
-  });
+    reader.onload = e => $(`#preview${idName}`).attr('src', e.target.result)
+    reader.readAsDataURL(en.target.files[0]);
+  }
+  let imagesArea = document.querySelectorAll(".productBody__main__form__image__box__form__set__field");
+  imagesArea.forEach((image, i) => image.onchange = e => addImage(i, e));
 });
+
+
