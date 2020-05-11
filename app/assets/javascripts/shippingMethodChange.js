af89272cfc259acf59c0862f92fce1271ef6b89d
shun255454 <donp3461@gmail.com>
2020-05-11 13:52:40 +0900

    Shipping-method-change
    配送方法を選択するとそれに合わせた選択肢が出てくる

--- app/assets/javascripts/shippingMethodChange.js
+++ app/assets/javascripts/shippingMethodChange.js
@@ -4,18 +4,17 @@ window.addEventListener("load", function () {
   let shippingArea_buyer = document.querySelector(".productBody__main__form__method__field__buyer");
   let shippingArea_exhibitor = document.querySelector(".productBody__main__form__method__field__exhibitor");
 
-  methodArea.onchange = function() {
-    console.log(methodArea.value);
+  function previewShippingMethod(){
     if(methodArea.value === 'true') {
       console.log(shippingArea)
-      shippingArea_buyer.disabled = false
-      shippingArea_exhibitor.disabled = true
+      shippingArea_buyer.disabled = true
+      shippingArea_exhibitor.disabled = false
       shippingArea.style.display = 'block';
       shippingArea_exhibitor.style.display = 'block';
       shippingArea_buyer.style.display = 'none';
     } else if(methodArea.value === 'false') {
-      shippingArea_exhibitor.disabled = false
-      shippingArea_buyer.disabled = true
+      shippingArea_exhibitor.disabled = true
+      shippingArea_buyer.disabled = false
       shippingArea.style.display = 'block';
       shippingArea_buyer.style.display = 'block';
       shippingArea_exhibitor.style.display = 'none';
@@ -23,36 +22,8 @@ window.addEventListener("load", function () {
       shippingArea.style.display = 'none';
     }
   }
-
-
-  // let shippingArea = document.querySelector(".productBody__main__form__method");
-  // let methodArea = document.querySelector("#item_shipper");
-  // let exhibitorArea = document.querySelector(".productBody__main__form__method__field__exhibitor");
-  // let buyerArea = document.querySelector(".productBody__main__form__method__field__buyer");
-  // methodArea.onchange = function() {
-  //   console.log(methodArea.value);
-  //   if(methodArea.value == 'true') {
-  //     exhibitorArea.style.display = 'block';
-  //   } else if(methodArea.value == 'false'){
-  //     buyerArea.style.display = 'block';
-  //   } else {
-  //     shippingArea.style.display = 'none';
-  //   }
-  // }
-
-  // let shippingArea = $(".productBody__main__form__method");
-  // let methodArea = $("#item_shipper");
-  // let exhibitorArea = $(".productBody__main__form__method__field__exhibitor");
-  // let buyerArea = $(".productBody__main__form__method__field__buyer");
-  // $('methodArea').change = function() {
-  //   console.log(methodArea.value);
-  //   if( methodArea.value == 'true') {
-  //     exhibitorArea.css('display') == 'block'
-  //   } else if(methodArea.value == 'false') {
-  //     buyerArea.css('display') == 'block'
-  //   } else {
-  //     shippingArea.css('display') == 'none'
-  //   }
-  // }
-
+  methodArea.onchange = function() {
+    previewShippingMethod()
+  }
+  previewShippingMethod()
 })
